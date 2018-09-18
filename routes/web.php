be5742fd904cb7fb4c2718c9d52431c8346309fd
QwisoDev <>
2018-09-18 10:05:56 -0400

    removing test route

--- routes/web.php
+++ routes/web.php
@@ -7,41 +7,6 @@ Route::get('/', function () {
     return view('pages.main');
 });
 
-Route::get('test', function(){
-    $fileData = function() {
-        $file = fopen('C:\repos\aquaristpages\storage\app\public\US.txt', 'r');
-
-        if (!$file)
-            die('file does not exist or cannot be opened');
-
-        while (($line = fgets($file)) !== false) {
-            yield $line;
-        }
-
-        fclose($file);
-    };
-
-    foreach ($fileData() as $line) {
-        $data = explode("\t", $line);
-
-        $newZip = [
-            'country' => $data[0],
-            'zipcode' => $data[1],
-            'city' => $data[2],
-            'state' => $data[3],
-            'state_abbr' => $data[4],
-            'county' => $data[5],
-            'lat' => $data[9],
-            'lon' => $data[10]
-        ];
-
-        \App\Zipcode::create($newZip);
-
-        echo "<br>";
-        print_r($newZip);
-    }
-});
-
 Route::get('logout', function(){
     if (auth()->check())
         auth()->logout();
