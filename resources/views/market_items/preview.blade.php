1ec985a85258aea0ab2a8b414a39bd488beac259
QwisoDev <>
2018-09-19 12:40:21 -0400

    styling text

--- resources/views/market_items/preview.blade.php
+++ resources/views/market_items/preview.blade.php
@@ -1,13 +1,13 @@
 <div class="market-item d-xs-flex d-sm-inline-block shadow mr-3 mb-3" style="min-width: 245px; max-width: 245px; min-height: 305px; max-height: 305px;">
     @if(auth()->id() == $item->user_id)
         <div class="row justify-content-end pr-3">
-            <div class="position-absolute btn btn-xs" onclick="console.log(this)"><i class="fa fa-edit"></i></div>
+            <div class="position-absolute btn btn-mine btn-xs p-2" onclick="console.log(this)"><i class="fa fa-edit"></i></div>
         </div>
     @endif
     <div style="overflow: hidden; display: flex; align-items: center; min-width: 245px; max-width: 245px; min-height: 250px; max-height: 250px;">
         <a href="{{url('marketplace/item')}}/{{$item->uuid}}"><img src="{{$item->media_url}}" width="245px"></a>
     </div>
-    <div class="col text-center pb-2">
+    <div class="col pb-2">
         <small><b>{{$item->title}}</b></small>
         <br/>
         <small class="text-muted">{{$item->created_at->diffForHumans() . ' ‧ ' . $item->zipcode->city . ', ' . $item->zipcode->state_abbr}}</small>
--- resources/views/market_items/show.blade.php
+++ resources/views/market_items/show.blade.php
@@ -4,7 +4,7 @@
 <div class="col d-flex justify-content-center">
     <img class="img-fluid" data-source="{{$item->media_url}}">
 </div>
-<div class="col text-center pb-2">
+<div class="col pb-2">
     <p class="m-0"><b>{{$item->title}}</b></p>
     <small>from <a href="{{url('profile')}}/{{$item->user->id}}">{{$item->user->name}}</a> in {{$item->zipcode->city}}, {{$item->zipcode->state_abbr}}</small>
 </div>
