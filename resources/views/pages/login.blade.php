4a0b2b0dfc0e041e8cb973bd6a00b50cec4c2628
QwisoDev <>
2018-09-19 10:49:09 -0400

    login button

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -6,7 +6,7 @@
         <h3>Welcome to Aquarist Pages</h3>
         <p>Please log in to continue</p>
         <div class="row justify-content-center">
-            <a class="col-4 btn btn-facebook btn-block text-white" href="{{url('facebook/login')}}">Continue with <i class="fab fa-facebook"></i></a>
+            <a class="col-sm-4 btn btn-facebook btn-block text-white" href="{{url('facebook/login')}}">Continue with <i class="fab fa-facebook"></i></a>
         </div>
     </div>
     <div class="col"></div>
