3ebf8a43a6e2ef8b31fda931a4efe491a2de22f3
QwisoDev <>
2018-09-21 19:42:00 -0400

    better login view

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -1,16 +1,15 @@
 @extends('templates.main')
 
 @section('content')
-    <div class="col"></div>
-    <div class="col text-center">
-        <h3>Welcome to Aquarist Pages</h3>
-        <p>Please log in to continue</p>
-        <div class="row justify-content-center">
-            <a class="col-sm-4 btn btn-facebook btn-block text-white" href="{{url('facebook/login')}}">Continue with <i class="fab fa-facebook"></i></a>
-        </div>
-        <div class="row justify-content-center pt-3">
-            <a class="col-sm-4 btn btn-success btn-block text-white" href="{{url('recruitment')}}">Want to help test? <i class="fa fa-code"></i></a>
+    <div class="d-flex text-center justify-content-center">
+        <div class="col">
+            <h3>Welcome to Aquarist Pages</h3>
+            <div class="d-flex justify-content-center pt-3">
+                <a class="col-sm-4 btn btn-facebook btn-block text-white" href="{{url('facebook/login')}}">Continue with <i class="fab fa-facebook"></i> if you dare</a>
+            </div>
+            <div class="d-flex justify-content-center pt-3">
+                <a class="col-sm-4 btn btn-success btn-block text-white" href="{{url('recruitment')}}">Find out how to test or develop <i class="fa fa-user"></i> <i class="fa fa-code"></i></a>
+            </div>
         </div>
     </div>
-    <div class="col"></div>
 @endsection
