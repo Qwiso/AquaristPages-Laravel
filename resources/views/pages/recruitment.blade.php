a6e3456b25f57acaac69c12eb5173c33cd9fbdc3
QwisoDev <>
2018-09-21 19:28:06 -0400

    again better

--- resources/views/pages/recruitment.blade.php
+++ resources/views/pages/recruitment.blade.php
@@ -1,9 +1,9 @@
 @extends('templates.main')
 
 @section('content')
-    <h2>Call For Testers</h2>
-    <div class="row pt-2">
-        <div class="col">
+    <div class="row justify-content-center">
+        <div class="col-8 bg-white py-3 border">
+            <h2>Call For Testers</h2>
             <p>
                 Are you interested in helping to test, possibly develop, this aquarist focused website?
                 <br>
@@ -11,16 +11,13 @@
                 <br>
                 <small><b>All feedback considered</b></small>
             </p>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="w-100 text-center">
-            <small><b>Contact Me</b> - click to reveal url or email</small>
-        </div>
-        <div class="col d-flex justify-content-center pt-2">
-            <div class="btn btn-facebook mx-3" onclick="togglePopover(this)" data-for="facebook" data-placement="bottom" data-content="https://www.facebook.com/zjones5487"><i class="fab fa-facebook"></i> Facebook</div>
-            <div class="btn btn-secondary mx-3" onclick="togglePopover(this)" data-for="email" data-trigger="manual" data-placement="bottom" data-content="qwisodev@gmail.com"><i class="fa fa-envelope"></i> @gmail.com</div>
+            <div class="w-100 text-center">
+                <small><b>Contact Me</b> - click to reveal url or email</small>
+            </div>
+            <div class="w-100 d-flex justify-content-center pt-2">
+                <div class="btn btn-facebook mx-3" onclick="togglePopover(this)" data-for="facebook" data-placement="bottom" data-content="https://www.facebook.com/zjones5487"><i class="fab fa-facebook"></i> Facebook</div>
+                <div class="btn btn-secondary mx-3" onclick="togglePopover(this)" data-for="email" data-trigger="manual" data-placement="bottom" data-content="qwisodev@gmail.com"><i class="fa fa-envelope"></i> @gmail.com</div>
+            </div>
         </div>
     </div>
     <script>
