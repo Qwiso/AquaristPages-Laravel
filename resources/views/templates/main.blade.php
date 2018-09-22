f159769edae5583ca79ccc13ac1357733cef7948
QwisoDev <>
2018-09-21 23:22:08 -0400

    crediting original icon image source

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -27,6 +27,7 @@
     <meta name="og:type" content="website">
 
     @yield('html-head')
+    <!-- original svg located at https://openclipart.org/detail/84031/fish -->
     <link rel="shortcut icon" href="{{asset('favicon.ico')}}"/>
     <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link rel="stylesheet=" href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
