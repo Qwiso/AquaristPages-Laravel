83d3c55d5f71b07c5fa37298ec1af690ef3b0062
QwisoDev <>
2018-09-22 17:17:42 -0400

    square images

--- resources/views/market_items/preview.blade.php
+++ resources/views/market_items/preview.blade.php
@@ -1,4 +1,4 @@
-<div class="market-item shadow mr-3 mb-3" style="min-width: 245px; max-width: 245px; min-height: 305px; max-height: 305px;">
+<div class="market-item shadow mr-3 mb-3" style="min-width: 250px; max-width: 250px; min-height: 305px; max-height: 305px;">
     @if(auth()->id() == $item->user_id)
     <div class="row justify-content-end pr-3">
         <div class="position-absolute btn btn-secondary pt-0 pb-1 px-1" onclick="editItem({{$item->id}})"><i class="fa fa-xs fa-edit"></i></div>
@@ -7,7 +7,7 @@
     <div class="row justify-content-start pl-3">
         <div class="position-absolute badge badge-secondary p-2 align-middle">${{$item->price == intval($item->price) ? intval($item->price) : $item->price}}</div>
     </div>
-    <div style="overflow: hidden; display: flex; align-items: center; justify-content: center; min-width: 245px; max-width: 245px; min-height: 250px; max-height: 250px;">
+    <div style="overflow: hidden; display: flex; align-items: center; justify-content: center; min-width: 250px; max-width: 250px; min-height: 250px; max-height: 250px;">
         {{--<a href="{{url('marketplace/item')}}/{{$item->uuid}}"><img src="{{asset('market_images/'.$item->uuid.'.png')}}" width="245px"></a>--}}
         <a style="cursor: pointer;" onclick="loadMarketItem('{{$item->uuid}}')"><img src="{{asset('market_images/'.$item->uuid.'.png')}}"></a>
     </div>
