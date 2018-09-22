380314764c6abc4e5d42d4c385a9f4c3bb804777
QwisoDev <>
2018-09-22 08:19:04 -0400

    env value name

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('env') != "local")
+        if (env('APP_ENV') == "dev")
             \URL::forceScheme('https');
     }
 
