6f993d07771fd1fbcfa91aeea951859349eb196f
QwisoDev <>
2018-09-22 10:21:52 -0400

    better map url - marketplace filters

--- resources/views/market_items/modal.blade.php
+++ resources/views/market_items/modal.blade.php
@@ -29,7 +29,7 @@
                             </p>
 
                             <div style="width:300px;height:150px;overflow:hidden;position:relative">
-                                <a href="https://www.google.com/maps/@\{{$item->zipcode->lat}},{{$item->zipcode->lon}},14z" target="_blank">
+                                <a href="https://www.google.com/maps/@?api=1&map_action=map&center={{$item->zipcode->lat}},{{$item->zipcode->lon}}&zoom=14" target="_blank">
                                     <div class="overlay-circle" style="top:0;left:75px;width:150px;height:150px;position:absolute;box-shadow:0 0 0 500px rgba(0,0,0,0.25);border-radius:500px;z-index:1000"></div>
                                     <div class="overlay-behind" style="width:100%;height:100%;position:absolute;top:0;left:0;">
                                         <img class="img-fluid d-block mx-auto" src="{{$radius_map}}">
--- resources/views/pages/marketplace.blade.php
+++ resources/views/pages/marketplace.blade.php
@@ -2,19 +2,35 @@
 
 @section('content')
 <div class="row">
-    <div class="col-md-2">
-        <form class="form-group" id="form_setMarketplaceSearchRadius">
-            <select onchange="this.form.submit()" name="search_radius" class="form-control">
-                <option {{request('search_radius') == 50 ? "selected" : ""}} value="50">50 mi</option>
-                <option {{request('search_radius') == 100 ? "selected" : ""}} value="100">100 mi</option>
-                <option {{request('search_radius') == 500 ? "selected" : ""}} value="500">500 mi</option>
-                <option {{request('search_radius') == "state" ? "selected" : ""}} value="state">State</option>
-                <option {{request('search_radius') == "country" ? "selected" : ""}} value="country">Country</option>
-            </select>
+    <div class="col">
+        <form id="form_marketplaceSearchFilter">
+            <div class="row">
+                <div class="col pr-1">
+                    <select name="search_category" class="form-control">
+                        <option value="">All</option>
+                        @foreach(DB::table('categories')->get() as $category)
+                            <option {{request('search_category') == $category->name ? "selected" : ""}} value="{{$category->name}}">{{ucwords($category->name)}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col pr-1">
+                    <select name="search_radius" class="form-control">
+                        <option {{request('search_radius') == 50 ? "selected" : ""}} value="50">50 mi</option>
+                        <option {{request('search_radius') == 100 ? "selected" : ""}} value="100">100 mi</option>
+                        <option {{request('search_radius') == 500 ? "selected" : ""}} value="500">500 mi</option>
+                        <option {{request('search_radius') == "state" ? "selected" : ""}} value="state">State</option>
+                        <option {{request('search_radius') == "country" ? "selected" : ""}} value="country">Country</option>
+                    </select>
+                </div>
+                <div class="col">
+                    <button type="submit" class="btn btn-primary">Apply</button>
+                </div>
+            </div>
         </form>
     </div>
 </div>
-<div class="row">
+
+<div class="row pt-3">
     <div class="col">
         @include('market_items.list', ['items'=>$localItems])
     </div>
