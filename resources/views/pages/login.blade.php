3c54f36b03161760134df39449268eee3d399fc9
QwisoDev <>
2018-09-21 23:10:15 -0400

    rewording call to test

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -8,7 +8,7 @@
                 <a class="col-sm-4 btn btn-facebook btn-block text-white" href="{{url('facebook/login')}}">Continue with <i class="fab fa-facebook"></i> if you dare</a>
             </div>
             <div class="d-flex justify-content-center pt-3">
-                <a class="col-sm-4 btn btn-success btn-block text-white" href="{{url('recruitment')}}">Find out how to test or develop <i class="fa fa-user"></i> <i class="fa fa-code"></i></a>
+                <a class="col-sm-4 btn btn-success btn-block text-white" href="{{url('recruitment')}}">Help me test or develop <i class="fa fa-user"></i> <i class="fa fa-code"></i></a>
             </div>
         </div>
     </div>
